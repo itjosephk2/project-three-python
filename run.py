9039352d3eb7b6c49783d5b0555852c4b6a1d38d
Joseph Keane <itjosephk2@users.noreply.github.com>
2024-01-24 14:49:12 +0000

    Feat: Added Fighter Class

--- run.py
+++ run.py
@@ -1,3 +1,23 @@
+class Fighter:
+    """
+    Class for fighter
+    """
+    energy_blast_counter = 0
+
+    def __init__(
+            self, name: str, hp: int,
+            attack: int, defense: int, special: int, speed: int
+    ):
+        self.name = name
+        self.hp = hp
+        self.hp_remaining = hp
+        self.attack = attack
+        self.defense = defense
+        self.special = special
+        self.speed = speed
+
+
+
 if __name__ == "__main__":
     clear()
     draw_line()
