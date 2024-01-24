3580251c50e45e74f3ea8974f970f8d2bd9eb86b
Joseph Keane <itjosephk2@users.noreply.github.com>
2024-01-24 14:56:00 +0000

    feat: Chapter 1 of the main story

--- run.py
+++ run.py
@@ -171,6 +171,47 @@ def draw_goku():
     draw_line()
     input("> Continue")
 
+def goku_meets_yamcha():
+    clear()
+    draw_line()
+    print("  Goku and krillin are travelling through the dessert.")
+    print("  Krillin: Goku where are we going?")
+    print("  Goku: I'm hungry!!")
+    print("  Krillin: Me too, But seriously where are we going?")
+    print("  Goku: I'm not sure krillin.")
+    print("  Master-Roshi just sent us out to"
+          " get him something from the ox king.")
+    print("  Krillin: Look over there !!")
+    print("  An oasis there might be food near the water.")
+    print("  Narrator: Goku and Krillin "
+          "stumble across a juicy leg of meat.")
+    print("  It almost looks too god to be true!!")
+    print("  Have they stumbled upon a life saving meal.")
+    print("  or have they come across "
+          "some trap left out by evil poachers.")
+    draw_line()
+    input('> Continue')
+    clear()
+    draw_line()
+    print("  Goku runs Towards the food.")
+    print("  Just as he goes to pick u"
+          "p the food a man jumps out.")
+    print("  Anonymous: Hey! WHat are "
+          "you doing! My name is Yamcha.")
+    print("  Yamcha: This is my trap "
+          "for catching sojme larger food!")
+    print("  Goku: I..")
+    print("  Yamcha Interupts Goku.")
+    print("  Yamcha: Don't you dare steal my food kid.")
+    print("  Yamcha: Let's fight to s"
+          "ettle this.")
+    print("  Krillin: Don't do this Go"
+          "ku it could be very dangerous.")
+    print("  Goku: Let's go Yamcha, I n"
+          "ever back down from a challenge.")
+    draw_line()
+    input('> Continue')
+
 if __name__ == "__main__":
     clear()
     draw_line()
